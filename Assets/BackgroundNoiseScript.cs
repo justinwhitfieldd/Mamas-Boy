45d8666634e91627b1a875d3a0a8c44e046f8128
Matthew Lane <matthewglenn@protonmail.com>
2024-02-28 21:11:52 -0600

    Add background music to SampleScene, add random clank script, make test alien faster

---
+++ Assets/BackgroundNoiseScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundNoiseScript : MonoBehaviour
+{
+    private float timer = 0;
+    public float noiseRateEarliest = 15;
+    public float noiseRateLatest = 30;
+    private float nextNoiseTimer;
+    private AudioSource clankNoise;
+    public AudioClip[] clankNoises;
+
+
+    private void Awake()
+    {
+        clankNoise = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextNoiseTimer = Random.Range(noiseRateEarliest, noiseRateLatest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < nextNoiseTimer)
+            timer += Time.deltaTime;
+        else
+        {
+            Debug.Log("Playing clank sound.");
+            clankNoise.clip = clankNoises[Random.Range(0, clankNoises.Length)];
+            clankNoise.PlayOneShot(clankNoise.clip);
+            timer = 0;
+            nextNoiseTimer = Random.Range(noiseRateEarliest, noiseRateLatest);
+        }
+    }
+}
