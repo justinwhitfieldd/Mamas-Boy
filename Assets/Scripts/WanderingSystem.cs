27fd22e8337e768bd02503dbd05650fc21979e74
Matthew Lane <matthewglenn@protonmail.com>
2024-03-03 16:46:41 -0600

    Add very rudimentary charge system

--- Assets/Scripts/WanderingSystem.cs
+++ Assets/Scripts/WanderingSystem.cs
@@ -5,11 +5,13 @@ using static UnityEngine.GraphicsBuffer;
 public class WanderingSystem : MonoBehaviour
 {
     public GameObject player;
-    public float jumpScareRadius = 3.0f;
+    public float jumpScareRadius = 1.0f;
+    public float alienFOV = 25f;
+    public float triggerRadius = 2.0f;
+    public LayerMask obstacleLayer;
     public AudioClip jumpScareSound;
     public GameObject ambienceSystem;
     public GameObject startingPoint;
-    public float speed = 1.0f;
     public float rotationSpeed = 5.0f;
     public float waitTimer = 5.0f;
     public float gravity = 10f;
@@ -17,6 +19,7 @@ public class WanderingSystem : MonoBehaviour
     public AudioClip alienStepL;
     public AudioClip alienStepR;
     public float stepCadence = 1.0f;
+    public float runCadence = 0.5f;
 
     private CharacterController characterController;
     private Animator animator;
@@ -29,12 +32,15 @@ public class WanderingSystem : MonoBehaviour
     private bool leftFoot = true;
     private bool jumpScared = false;
     private float jumpScareRotationSpeed = 25.0f;
+    private FPSController playerFPSController;
+    private bool playerSeen = false;
 
     void Start()
     {
         alienNoise = GetComponent<AudioSource>();
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        playerFPSController = player.GetComponent<FPSController>();
         previousPosition = transform.position;
         currentPoint = startingPoint;
         currentTransform = currentPoint.transform;
@@ -44,12 +50,12 @@ public class WanderingSystem : MonoBehaviour
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
+        #region Handles Jump Scaring Player
         if (distanceToPlayer < jumpScareRadius)
         {
             ambienceSystem.SetActive(false);
             animator.SetBool("Walking", false);
 
-            FPSController playerFPSController = player.GetComponent<FPSController>();
             playerFPSController.canMove = false;
 
             Quaternion playerRotation = Quaternion.LookRotation(transform.position - player.transform.position);
@@ -67,8 +73,35 @@ public class WanderingSystem : MonoBehaviour
 
             return;
         }
+        #endregion
 
-        bool atTransform = moveToTransform(currentTransform);
+        #region Handles Charging to Player
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+
+        RaycastHit hit;
+        bool visible = !Physics.Raycast(transform.position, directionToPlayer.normalized, out hit, distanceToPlayer, obstacleLayer);
+
+
+        if (visible && ((angleToPlayer < alienFOV) || (distanceToPlayer < triggerRadius)))
+        {
+            bool atPlayer = moveToTransform(player.transform, (2 * playerFPSController.runSpeed) / 3);
+            if (!atPlayer) makeFootStep(runCadence);
+
+            if (!playerSeen) makeNoise();
+            playerSeen = true;
+            
+            return;
+        }
+        else
+        {
+            if (playerSeen) makeNoise();
+            playerSeen = false;
+        }
+        #endregion
+
+        #region Handles Wandering to Points
+        bool atTransform = moveToTransform(currentTransform, playerFPSController.walkSpeed / 3);
 
         if (atTransform)
         {
@@ -83,13 +116,14 @@ public class WanderingSystem : MonoBehaviour
         }
         else
         {
-            makeFootStep();
+            makeFootStep(stepCadence);
         }
+        #endregion
 
-        previousPosition = transform.position; // Update previous position for the next frame
+        previousPosition = transform.position;
     }
 
-    bool moveToTransform(Transform transfromToMove)
+    bool moveToTransform(Transform transfromToMove, float speed)
     {
         Vector3 directionToTransform = (transfromToMove.position - transform.position).normalized;
         Vector2 flatPosition = new Vector2(transform.position.x, transform.position.z);
@@ -127,9 +161,9 @@ public class WanderingSystem : MonoBehaviour
         alienNoise.PlayOneShot(alienNoise.clip);
     }
 
-    void makeFootStep()
+    void makeFootStep(float cadence)
     {
-        if (stepTimer < stepCadence)
+        if (stepTimer < cadence)
             stepTimer += Time.deltaTime;
         else
         {
