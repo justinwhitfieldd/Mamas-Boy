89ab10f5f7e287fb99fd84ae84acb453fd176a0f
Matthew Lane <matthewglenn@protonmail.com>
2024-03-12 22:49:09 -0500

    Replace all doors with new doors, add alien and wander points
    Organize game objects a little
    Update door state on start
    Change door prefab to be open by default
    Make door debug log more verbose

--- Assets/door_open_close_files/doorOpenClose.cs
+++ Assets/door_open_close_files/doorOpenClose.cs
@@ -9,6 +9,11 @@ public class DoorOpenClose : MonoBehaviour
     public bool isDoorOpen = false;
     public Transform alien;
 
+    private void Start()
+    {
+        animator.SetBool("character_nearby", isDoorOpen);
+    }
+
     // Update is called once per frame
     private void Update()
     {
@@ -17,11 +22,11 @@ public class DoorOpenClose : MonoBehaviour
         if ((distanceToAlien < 4.0f) && (!isDoorOpen)) toggie();
     }
 
-
     public void toggie()
     {
         isDoorOpen = !isDoorOpen;
         animator.SetBool("character_nearby",isDoorOpen);
-        Debug.Log(message:"Opening door");
+        if (isDoorOpen) Debug.Log(message:"Opening " + name);
+        else Debug.Log(message: "Closing " + name);
     }
 }
